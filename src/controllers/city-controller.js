00b9d3ac007aec15c2e8be397f967de002c935c2
zeeshan <mdz31157@gmail.com>
2024-11-06 20:50:23 +0530

    initialized controllers

---
+++ src/controllers/city-controller.js
@@ -0,0 +1,24 @@
+const { CityService } = require('../services/index');
+
+const create = (req, res)=>{
+
+}
+
+const read = (req, res)=>{
+
+}
+
+const update = (req, res)=>{
+
+}
+
+const destroy = (req, res)=>{
+
+}
+
+module.exports ={
+    create,
+    read, 
+    update,
+    destroy
+}
---
+++ src/services/city-service.js
@@ -0,0 +1,65 @@
+const {CityRepository} = require('../repository/index');
+
+
+class CityService{
+
+    async createCity(data){
+
+        try {
+            const city = await CityRepository.createCity(data);
+            return city;
+
+        } catch (error) {
+
+            console.log("something went wrong in service layer");
+            throw {error};
+            
+        }
+
+    }
+
+    async updateCity(data, cityId){
+        try {
+            const city = await CityRepository.updateCity(data, cityId);
+            return city;
+            
+        } catch (error) {
+            console.log("something went wrong in service layer");
+            throw {error};
+        }
+    }
+
+
+    async removeCity(cityId){
+
+        try {
+            const city = await CityRepository.removeCity(cityId); 
+            return city;
+            
+        } catch (error) {
+            console.log("something went wrong in service layer");
+            throw {error};
+        }
+
+
+    }
+
+
+    async getCity(cityId)
+    {
+        try {
+            const city = await CityRepository.getCity(cityId);
+            return city;
+            
+        } catch (error) {
+            console.log("something went wrong in service layer");
+            throw {error};
+        }
+    }
+}
+
+
+module.exports = CityService;
+
+
+
